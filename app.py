54ee48f96502091119a7557944a0d9a5b21e8122
hinwork <yellowcrown0120@gmail.com>
2025-07-15 12:27:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -58,8 +58,16 @@ def chat():
     user = history[-1] if history else ""
     knee_label = None
     time_label = None
+    Ans_But_Not_Related = None
 
-    if not history:
+    end_words =  ['exit', 'quit', 'q', 'bye', '再見']
+    user_clean = user.strip().lower()
+
+    if user_clean in end_words:
+        return jsonify({"answer":"感謝查詢。","knee_label": None, "time_label": None})
+
+
+    if not history or user.strip() == "":
         reply = "歡迎，若想知道有關膝關節痛的資訊，請說出『我有膝痛』。"
         return jsonify({"answer": reply, "knee_label": None, "time_label": None})
 
@@ -69,30 +77,37 @@ def chat():
         time_label = predict_time(user)
 
     if knee_label == 1:
-        if time_label == 2:
+        if time_label !=1 and time_label != 0:
             reply = "請問你膝痛持續了多久？"
         elif time_label == 1:
             reply = ("知道你膝痛持續了三個月以上。\n"
                      "三個月以上是屬於慢性疼痛，以下的為您介紹一些運動影片，請在沒有疼痛的情況下做。\n"
                      "若身體有不適或情況變得嚴重，請諮詢醫生的意見。\n"
                      "#RecommandChronic")
+                     time_label = 2
+                     Ans_But_Not_Related = None
         elif time_label == 0:
             reply = ("知道你膝痛持續了三個月內。\n"
                      "三個月內是屬於急性疼痛，以下的為您介紹一些運動影片，請在沒有疼痛的情況下做。\n"
                      "若身體有不適或情況變得嚴重，請諮詢醫生的意見。\n"
                      "#RecommandAcute")
+                     time_label = 2
+                     Ans_But_Not_Related = None
         else:
             reply = "謝謝你的資訊。"
-    else:
+    elif Ans_But_Not_Related = 1:
         if user in ['yes', '是', '是的', '有']:
             reply = "請問你膝痛持續了多久？"
             knee_label = 1
+            Ans_But_Not_Related = 0
         elif user in ['no', '否', '沒有', '不是', '不', '沒']:
             reply = ("抱歉，我只能夠處理膝關節痛的問題。"
                      "若想知道有關膝關節痛的資訊，請說出『我有膝痛』。")
         else:
             reply = ("抱歉，我只能夠處理膝關節痛的問題，"
                      "請問你是否有膝關節不適嗎？")
+    else
+        Ans_But_Not_Related = 1
 
     return jsonify({
         "answer": reply,
@@ -102,4 +117,3 @@ def chat():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
-
