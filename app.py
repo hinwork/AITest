2ac909b323493d02d252bc0c09e05117655ae2d8
hinwork <yellowcrown0120@gmail.com>
2025-07-15 12:54:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def chat():
     user = history[-1] if history else ""
     knee_label = None
     time_label = None
-    Ans_But_Not_Related = None
+
 
     end_words =  ['exit', 'quit', 'q', 'bye', '再見']
     user_clean = user.strip().lower()
@@ -85,29 +85,28 @@ def chat():
                      "若身體有不適或情況變得嚴重，請諮詢醫生的意見。\n"
                      "#RecommandChronic")
             time_label = 2
-            Ans_But_Not_Related = None
+            
         elif time_label == 0:
             reply = ("知道你膝痛持續了三個月內。\n"
                      "三個月內是屬於急性疼痛，以下的為您介紹一些運動影片，請在沒有疼痛的情況下做。\n"
                      "若身體有不適或情況變得嚴重，請諮詢醫生的意見。\n"
                      "#RecommandAcute")
             time_label = 2
-            Ans_But_Not_Related = None
         else:
             reply = "謝謝你的資訊。"
-    elif Ans_But_Not_Related = 1:
+    else:
+        reply = ("抱歉，我只能夠處理膝關節痛的問題，"
+                     "請問你是否有膝關節不適嗎？")
         if user in ['yes', '是', '是的', '有']:
             reply = "請問你膝痛持續了多久？"
             knee_label = 1
-            Ans_But_Not_Related = 0
         elif user in ['no', '否', '沒有', '不是', '不', '沒']:
             reply = ("抱歉，我只能夠處理膝關節痛的問題。"
                      "若想知道有關膝關節痛的資訊，請說出『我有膝痛』。")
         else:
             reply = ("抱歉，我只能夠處理膝關節痛的問題，"
                      "請問你是否有膝關節不適嗎？")
-    else
-        Ans_But_Not_Related = 1
+
 
     return jsonify({
         "answer": reply,
